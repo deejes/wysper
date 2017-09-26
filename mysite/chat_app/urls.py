3f201f3d52d7653aa0c87dc615dec2526f1ef22b
Devansh Jain <j.devansh29@gmail.com>
2017-09-25 21:29:43 -0700

    adding files from tester

---
+++ mysite/chat_app/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import include,url
+from . import views
+
+from django.conf.urls import url, include
+from django.contrib.auth import views as auth_views
+
+#from mysite.core import views as core_views
+
+
+urlpatterns = [
+    url(r'^$', views.home, name='home'),
+    url(r'^login/$', auth_views.login, {'template_name': 'chat_app/login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
+    url(r'^signup/$', views.signup, name='signup'),
+    url(r'^index',views.index,name='index'),
+]
---
+++ mysite/chat_app/views.py
@@ -0,0 +1,36 @@
+# Create your views here.
+
+
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm,User
+from django.shortcuts import render, redirect
+
+
+
+
+def index(request):
+     return render(request, 'chat_app/index.html')
+
+
+
+@login_required
+def home(request):
+    users = User.objects.all()
+    print(users)
+    return render(request, 'chat_app/home.html',{'users':users})
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'chat_app/signup.html', {'form': form})
