b7d9106274ca750fce93d9b653c392d3c5b7eaa2
Devansh Jain <j.devansh29@gmail.com>
2017-09-25 21:46:03 -0700

    add basic message model, sans foreign keys

--- mysite/chat_app/models.py
+++ mysite/chat_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Message(models.Model):
+    body = models.TextField()
+    date = models.DateTimeField()
+
+    def __string__(self):
+        return self.title
