4655c57fc07d244bea4f03436df75261088c144e
Devansh Jain <j.devansh29@gmail.com>
2017-09-26 20:13:22 -0700

    change message model to include sender and receiver

--- mysite/chat_app/models.py
+++ mysite/chat_app/models.py
@@ -1,9 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib import admin
 
 # Create your models here.
 class Message(models.Model):
     body = models.TextField()
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='person_who_sends')
     date = models.DateTimeField()
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __string__(self):
         return self.title
+
+
+@admin.register(Message)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
--- mysite/chat_app/views.py
+++ mysite/chat_app/views.py
@@ -10,15 +10,17 @@ from datetime import datetime
 
 
 def index(request):
+     import pdb
+     pdb.set_trace()
      if request.method == "POST":
           message_instance = Message.objects.create(body=request.POST['chat-msg'])
      return render(request, 'chat_app/index.html')
 
 def test(request):
-    if request.method == "POST":
-          message_instance = Message.objects.create(body=request.POST['chat-msg'], date = datetime.now())     
     import pdb
-#     pdb.set_trace()
+    pdb.set_trace()
+    if request.method == "POST":
+          message_instance = Message.objects.create(body=request.POST['chat-msg'], date = datetime.now(),sender = request.user)     
     messes = Message.objects.all
     mes = request.POST['chat-msg']
     return render(request, 'chat_app/test.html', {'mes':mes,'messes':messes})
