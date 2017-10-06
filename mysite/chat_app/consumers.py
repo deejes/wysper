c8dc61c27b34be62e8759a09eb5316a09350b06e
Devansh Jain <j.devansh29@gmail.com>
2017-10-05 23:45:57 -0700

    add chat group to consumer.py

--- mysite/chat_app/consumers.py
+++ mysite/chat_app/consumers.py
@@ -1,23 +1,18 @@
-from django.http import HttpResponse
-from channels.handler import AsgiHandler
-
-def http_consumer(message):
-    # Make standard HTTP response - access ASGI path attribute directly
-    response = HttpResponse("Hello world! You asked for %s" % message.content['path'])
-    # Encode that response into message format (ASGI)
-    for chunk in AsgiHandler.encode_response(response):
-        message.reply_channel.send(chunk)
-
-
 # In consumers.py
 from channels import Group
 
 # Connected to websocket.connect
 def ws_add(message):
-    # Accept the incoming connection
+    # Accept the connection
     message.reply_channel.send({"accept": True})
-    # Add them to the chat group
+    # Add to the chat group
     Group("chat").add(message.reply_channel)
+
+# Connected to websocket.receive
+def ws_message(message):
+    Group("chat").send({
+        "text": "[user] %s" % message.content['text'],
+    })
 
 # Connected to websocket.disconnect
 def ws_disconnect(message):
