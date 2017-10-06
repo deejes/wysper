5f0f3941e08df18c0f6d548777f9fa59ace86d52
Devansh Jain <j.devansh29@gmail.com>
2017-10-05 23:47:01 -0700

    add support for groups in routing.py

--- mysite/chat_app/routing.py
+++ mysite/chat_app/routing.py
@@ -1,5 +1,14 @@
 # In routing.py
+# from channels.routing import route
+# channel_routing = [
+#     route("http.request", "chat_app.consumers.http_consumer"),
+# ]
+
 from channels.routing import route
+from myapp.consumers import ws_add, ws_message, ws_disconnect
+
 channel_routing = [
-    route("http.request", "chat_app.consumers.http_consumer"),
+    route("websocket.connect", ws_add),
+    route("websocket.receive", ws_message),
+    route("websocket.disconnect", ws_disconnect),
 ]
