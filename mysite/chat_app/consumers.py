77e9b0d2bbdf0ee753e42445c7b7299ac894a321
Devansh Jain <j.devansh29@gmail.com>
2017-10-05 23:43:03 -0700

    add groups to consumer.py

--- mysite/chat_app/consumers.py
+++ mysite/chat_app/consumers.py
@@ -7,3 +7,18 @@ def http_consumer(message):
     # Encode that response into message format (ASGI)
     for chunk in AsgiHandler.encode_response(response):
         message.reply_channel.send(chunk)
+
+
+# In consumers.py
+from channels import Group
+
+# Connected to websocket.connect
+def ws_add(message):
+    # Accept the incoming connection
+    message.reply_channel.send({"accept": True})
+    # Add them to the chat group
+    Group("chat").add(message.reply_channel)
+
+# Connected to websocket.disconnect
+def ws_disconnect(message):
+    Group("chat").discard(message.reply_channel)
