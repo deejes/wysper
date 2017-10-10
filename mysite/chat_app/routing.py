c682417fd36ba999b4722cdb34cfe6b0caa1d5d3
Devansh Jain <j.devansh29@gmail.com>
2017-10-09 23:44:03 -0700

    routing for ws_message

--- mysite/chat_app/routing.py
+++ mysite/chat_app/routing.py
@@ -4,11 +4,18 @@
 #     route("http.request", "chat_app.consumers.http_consumer"),
 # ]
 
+# from channels.routing import route
+# from chat_app.consumers import ws_add, ws_message, ws_disconnect
+
+# channel_routing = [
+#     route("websocket.connect", ws_add),
+#     route("websocket.receive", ws_message),
+#     route("websocket.disconnect", ws_disconnect),
+# ]
+# In routing.py
 from channels.routing import route
-from chat_app.consumers import ws_add, ws_message, ws_disconnect
+from chat_app.consumers import ws_message
 
 channel_routing = [
-    route("websocket.connect", ws_add),
     route("websocket.receive", ws_message),
-    route("websocket.disconnect", ws_disconnect),
 ]
