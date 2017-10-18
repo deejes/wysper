4c00c4a50506abb6708f87cd08b38d98fa54f779
Devansh Jain <j.devansh29@gmail.com>
2017-10-18 16:29:28 -0700

    add group name to sockets consumers

--- mysite/chat_app/consumers.py
+++ mysite/chat_app/consumers.py
@@ -3,18 +3,19 @@ from channels import Group
 
 # Connected to websocket.connect
 def ws_add(message):
-    import pdb;pdb.set_trace()
+    #import pdb;pdb.set_trace()
     # print ("asd")
     # Accept the connection
     message.reply_channel.send({"accept": True})
     # Add to the chat group
-    Group("chat").add(message.reply_channel)
+    group_name = message.content['path'].split('/')[-1]
+    Group(group_name).add(message.reply_channel)
 
 # Connected to websocket.receive
 def ws_message(message):
-    # import pdb
-    # pdb.set_trace()
-    Group("chat").send({
+    import pdb;pdb.set_trace()
+    group_name = message.content['path'].split('/')[-1]
+    Group(group_name).send({
         "text": "[user] %s" % message.content['text'],
     })
 
