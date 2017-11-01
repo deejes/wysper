606402713cb70bed91088ca2e54b4a96c92e4ffa
deejes <j.devansh29@gmail.com>
2017-11-01 00:41:33 -0700

    add byte fields in model to hold pickle public and private keys

--- mysite/chat_app/models.py
+++ mysite/chat_app/models.py
@@ -1,17 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib import admin
-
 # Create your models here.
 class Message(models.Model):
     body = models.TextField()
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='person_who_sends')
     date = models.DateTimeField()
     receiver = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __string__(self):
+    secret_key = models.BinaryField()
+    public_key = models.BinaryField()
+    def __string__(self): 
         return self.title
-
 
 @admin.register(Message)
 class AuthorAdmin(admin.ModelAdmin):
